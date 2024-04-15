f73a89a9b0448a7ad6bd9221934991f196196130
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2024-04-14 23:55:59 -0300

    init of personal information

---
+++ src/app/components/personalInformation/pi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-pi',
+    templateUrl: './pi.component.html',
+    styleUrls: ['./pi.component.css', '../../../styles.css']
+})
+
+export class AppPI {
+  title = 'my_portfolio_code';
+}
