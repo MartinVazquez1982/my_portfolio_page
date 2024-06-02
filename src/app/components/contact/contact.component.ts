47da7a1f82d59f172cb857ff1191b57451b60cb3
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2024-06-02 12:31:27 -0300

    advanced reponsive

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -21,11 +21,12 @@ export class ContactApp {
   
     async sendEmail(){
         emailjs.init('v6q0CQ4uopWNAmPDw');
-        let response = await emailjs.send("service_h67m3hn","template_bso8ral",{
+        await emailjs.send("service_h67m3hn","template_bso8ral",{
             from_name: this.form.value.from_name,
             from_email: this.form.value.from_email,
             from_subject: this.form.value.from_subject,
             message: this.form.value.message,
         });
+        this.form.reset();
     }
 }
